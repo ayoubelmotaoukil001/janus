a0e9e223eb2e995a2776001cfd268271b9ab82d8
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-03-13 14:22:59 +0000

    feat(User.php) edit add sanctum tokens support and define one-to-many relationships with habits and logs

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Laravel\Sanctum\HasApiTokens;
+use App\Models\Habit;
+use App\Models\HabitLog;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable  , HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +46,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function habits()
+    {
+        return $this->hasMany(Habit::class);
+    }
+    public function habitLogs()
+    {
+        return $this->hasMany(HabitLog::class);
     }
 }
